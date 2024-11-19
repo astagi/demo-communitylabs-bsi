5f73ea442716283af64fdda30fb80e981b3b09b4
Andrea Stagi <stagi.andrea@gmail.com>
2024-11-19 12:14:42 +0100

    chore: add note on js parts

--- src/main.js
+++ src/main.js
@@ -5,3 +5,9 @@ import "./main.scss"
 document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach((e) => {
     new Dropdown(e);
 })
+
+/*
+In questo esempio si nota come si può applicare in autonomia le funzionalità
+del Dropdown. Aggiungere in seguito le altre funzioni per attivare il JavaScript
+degli altri elementi.
+*/
