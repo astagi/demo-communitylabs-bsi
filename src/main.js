87dc807b755771eb4a6887d925e5c280694bbffd
Andrea Stagi <stagi.andrea@gmail.com>
2024-11-20 15:02:28 +0100

    fix: use standard input label

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import { Dropdown, NavBarCollapsible } from "bootstrap-italia"
+import { Dropdown, NavBarCollapsible, Input } from "bootstrap-italia"
 
 import "./main.scss"
 
 document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach((e) => {
-    new Dropdown(e);
+    new Dropdown(e)
 })
 
 const navBarCollapsibleElement = document.querySelector('#navC2')
 new NavBarCollapsible(navBarCollapsibleElement)
+
+const emailInputElement = document.querySelector('#emailInput')
+new Input(emailInputElement)
 
 /*
 In questo esempio si nota come si può applicare in autonomia le funzionalità
